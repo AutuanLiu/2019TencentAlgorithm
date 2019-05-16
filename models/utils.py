b00ed6d6f1b7d91203dd25f94ca62f42862982b0
AutuanLiu <autuanliu@163.com>
2019-05-16 20:36:29 +0800

    add  new files

--- models/utils.py
+++ models/utils.py
@@ -1,68 +1,56 @@
+import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-from deepctr.utils import VarLenFeat, SingleFeat
+
+from deepctr.utils import SingleFeat, VarLenFeat
 
 
 def sparse_feature_encoding(data, sparse_features_names):
     """稀疏编码函数"""
-    
+
     for feat in sparse_features_names:
         lbe = LabelEncoder()
         data[feat] = lbe.fit_transform(data[feat])
     return data
 
 
-def split(x, sep=',', key2index=None):
-    """处理多值特征,该函数主要做分离"""
+def single_multi_value_feature_encoding(data, feature, padding_func, key2index={}, sequence_dim=None, max_feature_length=None, combiner='mean', hashing=False):
+    """单个特征操作
     
-    key_ans = x.split(sep)
-    for key in key_ans:
-        if key not in key2index:
-            # Notice : input value 0 is a special "padding",so we do not use 0 to encode valid feature for sequence input
-            key2index[key] = len(key2index) + 1
-    return list(map(lambda x: key2index[x], key_ans))
+    Arguments:
+        data (pd.DataFrame): -- 原始数据
+        feature (str): -- 多值特征名
+        padding_func (function) -- padding 函数
+        max_feature_length (int) -- 自定义长度
+        sequence_dim (int) -- 序列特征维度
+        combiner (str) -- defaults to 'mean'
+    
+    Returns:
+        sequence_feature  -- padding 后的特征
+    """
+
+    def split(x, sep=','):
+        """处理多值特征,该函数主要做分离"""
+
+        if not isinstance(x, str):
+            x = str(x)
+        key_ans = x.split(sep)
+        for key in key_ans:
+            if key not in key2index:
+                key2index[key] = len(key2index) + 1
+        return list(map(lambda x: key2index[x], key_ans))
+
+    feature_list = list(map(split, data[feature].values))    # 分割
+    max_length = max_feature_length if max_feature_length else max(list(map(len, feature_list)))
+    # padding 对齐
+    padding_feature = padding_func(feature_list, max_length)
+    dim = sequence_dim if sequence_dim else len(key2index) + 1
+    sequence_feature = VarLenFeat(feature, dim, max_length, combiner, hash_flag=hashing, dtype="string")
+    return sequence_feature
 
 
-def single_multi_value_feature_encoding(data, multi_value_feature_name):
-    feature_list = list(map(split, data[multi_value_feature_name].values))
-    feature_length = np.array(list(map(len, feature_list)))
-    max_len = max(feature_length)
-    # Notice : padding=`post`
-    feature_list = pad_sequences(
-        feature_list,
-        maxlen=max_len,
-        padding='post',
-    )
-    return feature_list, max_len
+def sparse_feat_list_gen(data, sparse_features, mult=1, hashing=False):
+    return [SingleFeat(feat, data[feat].nunique() * mult, hash_flag=hashing, dtype='string') for feat in sparse_features]
 
 
-def multi_value_feature_encoding(data, multi_value_feature_name):
-    mulval_list = []
-    max_len_list = []
-    for feature in multi_value_feature_name:
-        feature_list, max_len = single_multi_value_feature_encoding(data, feature)
-        mul_val_list.append(feature_list)
-    return mul_val_list, max_len_list
-
-
-def sequence_feature_acquire(max_len_list, multi_value_feature_name, hashing):
-    key2index = {}
-    output = []
-    if hashing:
-        for max_len, feature in zip(max_len_list, multi_value_feature_name):
-            # Notice : value 0 is for padding for sequence input feature
-            sequence_feature = [VarLenFeat(feature, 100, max_len, 'mean', hash_flag=True, dtype="string")]
-            output.append(sequence_feature)
-    else:
-        for max_len, feature in zip(max_len_list, multi_value_feature_name):
-            # Notice : value 0 is for padding for sequence input feature
-            sequence_feature = [VarLenFeat(feature, len(key2index) + 1, max_len, 'mean')]
-            output.append(sequence_feature)
-    return output
-
-
-def sparse_feat_list_gen(data, sparse_feature, hashing):
-    if hashing:
-        sparse_feat_list = [SingleFeat(feat, data[feat].nunique() * 5, hash_flag=True, dtype='string') for feat in sparse_features]
-    else:
-        sparse_feat_list = [SingleFeat(feat, data[feat].nunique()) for feat in sparse_features]
-    return sparse_feat_list
+def dense_feat_list_gen(data, dense_features, hashing=False):
+    return [SingleFeat(feat, 0, hash_flag=hashing, dtype='string') for feat in dense_features]
