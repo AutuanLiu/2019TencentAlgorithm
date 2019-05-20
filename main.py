2de8a7fc0089bcfda0aed67d538b82ab7887b454
AutuanLiu <autuanliu@163.com>
2019-05-20 10:44:28 +0800

    update

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import tensorflow as tf
 from tensorflow.python.keras.callbacks import EarlyStopping
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tensorflow.python.keras.utils import plot_model
-
 from models import AdaBound, AdamW, CyclicLR, huber_loss
 from models.utils import *
 
@@ -17,9 +16,11 @@ path = './data/'
 
 # 数据读取
 train_data = pd.read_csv(f'{path}train_set_final.csv', low_memory=False, encoding='utf-8')
+valid_data = pd.read_csv(f'{path}valid_set_final.csv', low_memory=False, encoding='utf-8')
 test_data = pd.read_csv(f'{path}test_set_B.csv', low_memory=False, encoding='utf-8')
 
 # 变量类型
+backup_sparse_features = ['has_price']
 sparse_features = [
     'ad_id', 'ad_type_id', 'ad_account_id', 'item_id', 'item_type', 'is_all_field', 'has_product_id',
     'consuptionAbility', 'crt_dateYear', 'crt_dateMonth', 'crt_dateWeek', 'crt_dateDay', 'crt_dateDayofweek',
@@ -30,7 +31,8 @@ dense_features = ['price']
 multi_value_features = [
     'size', 'time', 'age', 'area', 'device', 'behavior', 'connectionType', 'gender', 'education', 'status', 'work'
 ]
-multi_value_features_cnt = [5, 7, 1000, 2000, 8, 500, 7, 5, 10, 17, 8]
+multi_value_features_cnt = [2, 7, 1000, 1800, 8, 500, 7, 5, 7, 8, 5]
+multi_value_features_emb_sz = [10, 93, 76, 171, 5, 348, 5, 4, 6, 8, 5]
 
 # 预测目标
 target = train_data['target']
@@ -60,11 +62,11 @@ model_settings = {
 # model_name = 'FGCNN'
 # model_settings = {
 #     "embedding_size": 8,
-#     "conv_kernel_width": (3, 3, 3),
-#     "conv_filters": (5, 8, 5),
-#     "new_maps": (3, 3, 3),
-#     "pooling_width": (2, 2, 2),
-#     "dnn_hidden_units": (128, ),
+#     "conv_kernel_width": (3, 3),
+#     "conv_filters": (5, 5),
+#     "new_maps": (3, 3),
+#     "pooling_width": (2, 2),
+#     "dnn_hidden_units": (256, 256),
 #     "l2_reg_embedding": 1e-5,
 #     "l2_reg_dnn": 0,
 #     "dnn_dropout": 0
@@ -84,6 +86,7 @@ model_settings = {
 # }
 ## !! TODO 关于模型的设置结束 !!
 
+## ! TODO 训练集数据处理
 # 数值编码
 # 稀疏特征
 train_data = sparse_feature_encoding(train_data, sparse_features)
@@ -91,6 +94,7 @@ sparse_feat_list = sparse_feat_list_gen(train_data, sparse_features, emb_rule=Tr
 
 # 连续特征 标准化处理
 train_data, scaler = dense_feature_scale(train_data, dense_features)
+valid_data, _ = dense_feature_scale(valid_data, dense_features, scaler)
 test_data, _ = dense_feature_scale(test_data, dense_features, scaler)
 # test_data.to_csv(f'{path}test_set_final1.csv', encoding='utf-8')
 dense_feat_list = dense_feat_list_gen(train_data, dense_features, hash_flag=cfg["hash_flag"])
@@ -108,6 +112,7 @@ sparse_input = [train_data[feat.name].values for feat in sparse_feat_list]
 dense_input = [train_data[feat.name].values for feat in dense_feat_list]
 model_input = sparse_input + dense_input + padding_feat_list
 feature_dim_dict = {"sparse": sparse_feat_list, "dense": dense_feat_list, "sequence": sequence_feat_list}
+# ! END TODO 训练集
 
 # 模型定义
 model = getattr(ctr, model_name)(feature_dim_dict, task='regression', **model_settings)
@@ -125,13 +130,39 @@ clr = CyclicLR(mode='triangular')
 early_stopping = EarlyStopping(monitor='val_loss', patience=10)
 ## TODO 优化器与策略配置结束
 
+## ! TODO 验证集 !
+# 数值编码
+# 稀疏特征
+valid_data = sparse_feature_encoding(valid_data, sparse_features)
+sparse_feat_list = sparse_feat_list_gen(valid_data, sparse_features, emb_rule=True, hash_flag=cfg["hash_flag"])
+
+# 连续特征
+dense_feat_list = dense_feat_list_gen(valid_data, dense_features, hash_flag=cfg["hash_flag"])
+
+# 序列特征
+padding_func = partial(pad_sequences, **padding_cfg)
+padding_feat_list = []
+for feature, emb_sz, cnt in zip(multi_value_features, multi_value_features_emb_sz, multi_value_features_cnt):
+    *ret, = single_multi_value_feature_encoding(valid_data, feature, padding_func, seq_dim=emb_sz, max_len=cnt, **cfg)
+    padding_feat_list.append(ret[1])
+
+# 模型输入
+sparse_input = [valid_data[feat.name].values for feat in sparse_feat_list]
+dense_input = [valid_data[feat.name].values for feat in dense_feat_list]
+valid_input = sparse_input + dense_input + padding_feat_list
+
+# 预测目标
+valid_target = valid_data['target']
+valid = valid_input, valid_target
+## ! END TODO 验证集 !
+
 # 模型拟合
 model.compile(adabound, huber_loss, metrics=['mse', "mae"])
 history = model.fit(model_input,
                     target,
                     batch_size=BATCH,
                     epochs=EPOCH,
-                    validation_split=0.1,
+                    validation_data=valid,
                     workers=CORES,
                     callbacks=[early_stopping])
 
@@ -148,8 +179,6 @@ model.save_weights(f'./saved/{model_name}_weights.h5')
 # plt.show()
 
 # !TODO ！预测
-multi_value_features_emb_sz = [10, 94, 76, 171, 5, 357, 5, 4, 6, 8, 5]
-
 # 数值编码
 # 稀疏特征
 test_data = sparse_feature_encoding(test_data, sparse_features)
